e046b21896f262b78d6796ccc86a072a3d40dd34
Guillaume Hammadi <ghammadi@gmail.com>
2015-02-22 01:08:02 +1100

    Displaying a rectangle with an image inside...

--- paperGlue/ViewController.swift
+++ paperGlue/ViewController.swift
@@ -7,17 +7,17 @@
 //
 
 import UIKit
-//class ViewController: UIViewController,UIAlertViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIPopoverControllerDelegate
-
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var imgOne:UIImageView!=nil
     @IBOutlet var imgTwo:UIImageView!=nil
+    @IBOutlet var resultImg:UIImageView!=nil
 
     var currentBtn: UIButton!=nil
     
-    @IBAction func buttonTapped(sender: UIButton) {
+    @IBAction func selectImage(sender: UIButton) {
         currentBtn = sender
         
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
@@ -28,6 +28,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.presentViewController(imag, animated: true, completion: nil)
         }
     }
+    
+    @IBAction func mergeImages(sender: UIButton) {
+        /* TODO
+        *   - Get size of image 1 and image 2
+        *   - Create empty image of:
+        *       -width = max(image 1 width, image 2 width)
+        *       - height = image 1 height + image 2 height
+        *   - position image 1 top left
+        *   - position image 2 bottom left
+        *   - PROFIT!!
+        */
+        
+        var imgOneSize: CGSize! = imgOne?.image?.size
+        var imgTwoSize: CGSize! = imgTwo?.image?.size
+        
+        var mergedImgHeight = imgOneSize.height + imgTwoSize.height
+        
+        var mergedImgSize: CGSize = CGSizeMake(mergedImgWidth(), mergedImgHeight)
+        
+        var rectangle = CGRectMake(0.0, 0.0, mergedImgWidth(), mergedImgHeight)
+        var imageRef = CGImageCreateWithImageInRect(imgOne.image?.CGImage, rectangle)
+        var mergedImg = UIImage(CGImage: imageRef)
+        resultImg.image = mergedImg
+    }
+    
+    func mergedImgWidth() -> CGFloat {
+        var imgOneSize: CGSize! = imgOne?.image?.size
+        var imgTwoSize: CGSize! = imgTwo?.image?.size
+        
+        let imgOneWidth: CGFloat! = imgOneSize!.width
+        println("imgOneWidth = \(imgOneWidth)")
+        
+        
+        let imgTwoWidth: CGFloat! = imgTwoSize.width
+        println("imgTwoWidth = \(imgTwoWidth)")
+        
+        return CGFloat(max(imgOneWidth!,imgTwoWidth!))
+    }
+    
+    
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject: AnyObject]) {
 
@@ -48,6 +88,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
         imgOne.contentMode = .ScaleAspectFit
         imgTwo.contentMode = .ScaleAspectFit
+        resultImg.contentMode = .ScaleAspectFit
     }
 
     override func didReceiveMemoryWarning() {
